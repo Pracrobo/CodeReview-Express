153336f995aec2e7e2fe79a35ce8bd660ffdbee6
gnxm37 <133202877+gnxm37@users.noreply.github.com>
2025-05-21 18:58:40 +0900

    feat:feature/login ongoing

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import session from 'express-session';
+import passport from 'passport';
+import authRoutes from './routes/auth.js';
+import dotenv from 'dotenv';
+dotenv.config();
+import './config/passport.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the GitHub OAuth Authentication App');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
