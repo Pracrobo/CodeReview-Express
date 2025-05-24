51225128b3d13d8a6ac7529140b77a796cc53416
Pracrobo <cpu180621@gmail.com>
2025-05-24 15:28:46 +0900

    fix: - 1. local에서 ssh 터널링 설정법 공유한대로 열기 - 2. DB env쪽 연결 오류 방지 -   Number - 3. pool 관련 코드 변경 함수화 제거 $ 연결확인 함수 추가

--- src/config/db_env.js
+++ src/config/db_env.js
@@ -4,7 +4,7 @@ dotenv.config();
 // db_env.js
 const config = {
   MYSQL_HOST: process.env.MYSQL_HOST,
-  MYSQL_PORT: process.env.MYSQL_PORT,
+  MYSQL_PORT: Number(process.env.MYSQL_PORT),
   MYSQL_USER: process.env.MYSQL_USER,
   MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
   MYSQL_DB: process.env.MYSQL_DB,
--- src/config/db_index.js
+++ src/config/db_index.js
@@ -1,24 +1,28 @@
 import mysql from "mysql2/promise";
 import config from "./db_env.js";
 
-function getConnectionPool() {
+const connection_pool = mysql.createPool({
+  host: config.MYSQL_HOST,
+  port: config.MYSQL_PORT,
+  user: config.MYSQL_USER,
+  password: config.MYSQL_PASSWORD,
+  database: config.MYSQL_DB,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// 연결 확인 함수
+export async function testConnection() {
   try {
-    const connection_pool = mysql.createPool({
-      host: config.MYSQL_HOST,
-      port: config.MYSQL_PORT,
-      user: config.MYSQL_USER,
-      password: config.MYSQL_PASSWORD,
-      database: config.MYSQL_DB,
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0
-    });
+    const conn = await connection_pool.getConnection();
     console.log("DB connected successfully.");
-    return connection_pool;
+    conn.release();
   } catch (err) {
     console.error("DB connection error:", err);
     throw err;
   }
 }
 
-export default getConnectionPool;
+export default connection_pool;
+testConnection();
