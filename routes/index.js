bf83ad6b8f3f44ab7c1a4e4e20f8edcef00e32fe
gnxm37 <133202877+gnxm37@users.noreply.github.com>
2025-05-29 10:18:43 +0900

    refactor: 결제 및 인증 관련 라우터 구조 개선

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,13 @@ import paymentRoute from './paymentRoute.js';
 
 const router = express.Router();
 
+// 인증 관련 라우트
 router.use('/auth', authRoutes);
+
+// 저장소 관련 라우트
 router.use('/repositories', repositoryRoutes);
+
+// 결제 및 구독 관련 라우트
 router.use('/payment', paymentRoute);
 
 export default router;
