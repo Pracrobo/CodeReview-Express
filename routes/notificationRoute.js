fe320f49c636181b1e6ef82e59a9a11f1b681a2c
c99-coder <c99@kakao.com>
2025-06-07 22:05:23 +0900

    refactor: authMiddleware 임포트 제거

--- routes/notificationRoute.js
+++ routes/notificationRoute.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { sseSetting } from '../controllers/notificationController.js';
-import { authenticate } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
